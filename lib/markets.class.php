3c5d3a90fc88d0ff820df1b0bc80386bee41266d
danda <danda@osc.co.cr>
2016-07-29 15:14:41 -0700

    add get_markets_with_trades()

--- lib/markets.class.php
+++ lib/markets.class.php
@@ -1,23 +1,46 @@
 <?php
 
 require_once( __DIR__ . '/strict_mode.funcs.php' );
+require_once( __DIR__ . '/currencies.class.php' );
 require_once( __DIR__ . '/trades.class.php' );
-require_once( __DIR__ . '/btcutil.class.php' );
 
 class markets {
     
     public function get_markets() {
-        $tradesobj = new trades();
-        $trades = $tradesobj->get_all_trades();
+        $currencies = new currencies();
+        $clist = $currencies->get_all_currencies();
     
         $markets = [];
-        foreach( $trades as $trade ) {
-            $pair = strtolower($trade['currency']) . '_' . 'btc';
-            $market = ['pair' => $pair];
+        foreach( $clist as $symbol => $c ) {
+            $pair = strtolower($symbol) . '_' . 'btc';
+            $market = ['pair' => $pair,
+                       'lname' => $c['name'],
+                       'rname' => 'Bitcoin',
+                       'name' => sprintf( "%s / %s", $c['name'], 'Bitcoin' )
+                      ];
             // maybe add more attributes later.
             $markets[$pair] = $market;
         }
-        ksort( $markets );
         return $markets;
+    }
+    
+    public function get_markets_with_trades() {
+        $t = new trades();
+        $all = $t->get_all_trades();
+        
+        $markets = $this->get_markets();
+        $traded = [];
+        foreach( $all as $trade ) {
+            $pair = strtolower($trade['currency']) . '_btc';
+            $traded[$pair] = $markets[$pair];
+        }
+        
+        ksort( $traded );
+        return $traded;
+    }
+    
+    public function get_market_name( $pair ) {
+        $m = $markets['pair'];
+        
     }
 }

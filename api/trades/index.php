27b9655f424b945ee19fc9f6296b4121d839ef34
danda <danda@osc.co.cr>
2016-07-29 17:00:21 -0700

    make trades API functional.  even though not used by frontend yet.

--- api/trades/index.php
+++ api/trades/index.php
@@ -1,41 +1,47 @@
 <?php
 
-$json_file = '/home/danda/.local/share/Bitsquare/mainnet/db/trade_statistics.json';
-mb_internal_encoding('UTF-8');
+require_once( __DIR__ . '/../../lib/summarize_trades.class.php');
 
 $market = @$_GET['market'];
+$format = @$_GET['format'];
+$prettyjson = @$_GET['prettyjson'];
 
 function bail($code, $msg) {
     header($_SERVER["SERVER_PROTOCOL"]." $code $msg", true, $code);
     die( $msg );
 }
 
-list($left) = @explode( '_', $market );  // BTC will always be right.  ( for now? )
-$left = 'DASH';
-if( !$market || !$left ) {
+if( !$market ) {
     bail( 404, "Not found" );
 }
-$left = strtoupper($left);
 
-// remove some garbage data at beginning of file, if present.
-$fh = fopen( $json_file, 'r' );
+$criteria = [
+    'market' => $market,
+    'datetime_from' => @$_GET['timestamp_from'],
+    'datetime_to' => @$_GET['timestamp_to'],
+    'direction' => @$_GET['direction'],
+    'limit' => @$_GET['limit'],
+    'sort' => @$_GET['sort'],
+    'integeramounts' => @$_GET['integeramounts'],
+    'fields' => @$_GET['fields'] ? explode(',', $_GET['fields'] ) : null
+];
 
-// we use advisory locking.  hopefully bitsquare does too?
-if( !$fh || !flock( $fh, LOCK_SH ) ) {
-    bail( 500, "Internal Server Error" );
-}
-$buf = stream_get_contents( $fh );
-fclose( $fh );
+$trades = new trades();
+$results = $trades->get_trades($criteria);
 
-$start = strpos( $buf, '[');
-$data = json_decode( substr($buf, $start), true );
-
-$matches = [];
-foreach( $data as $trade ) {
-    if( $trade['currency'] == $left) {
-        $matches[] = $trade;
+if( $format == 'csv' ) {
+    // serve response to client.
+    $fh = fopen( 'php://output', 'w');
+    if( count( $results ) ) {
+        fputcsv($fh, array_keys( $results[0] ) );
     }
+
+    foreach( $rows as $k => $row ) {
+        fputcsv( $fh, $row );
+    }
+    fclose( $fh );
+}
+else {
+    echo json_encode( $results, $prettyjson ? JSON_PRETTY_PRINT : null );
 }
 
-// serve JSON to client.
-echo json_encode($matches, JSON_PRETTY_PRINT);

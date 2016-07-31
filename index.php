21b951c0c9a0e38398179ce052402f5de919c4eb
danda <danda@osc.co.cr>
2016-07-31 08:20:34 -0700

    remove amcharts, add back highcharts

--- index.php
+++ index.php
@@ -95,11 +95,9 @@ $amcharts_cdn = 'https://www.amcharts.com/lib/3';
 <html>
 <head>
 <?php include( dirname(__FILE__) . '/widgets/head.html' ); ?>
-<script src="<?= $amcharts_cdn ?>/amcharts.js"></script>
-<script src="<?= $amcharts_cdn ?>/serial.js"></script>
-<script src="<?= $amcharts_cdn ?>/themes/light.js"></script>
-<script src="<?= $amcharts_cdn ?>/amstock.js"></script>
-<script src="<?= $amcharts_cdn ?>/plugins/dataloader/dataloader.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.9.1.js"></script>
+<script src="https://code.highcharts.com/stock/highstock.js"></script>
+<script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
 <?php require_once( dirname( __FILE__) . '/widgets/timezone-js.html' ); ?>
 <style>
 #trade_history {
@@ -111,8 +109,7 @@ $amcharts_cdn = 'https://www.amcharts.com/lib/3';
 #sell_orders td, #buy_orders td, #trade_history td {
     width: 100%;
 }
-#chartdiv {
-    visibility: hidden;
+#container {
     height: 500px;
 }
 #market_info td {
@@ -133,7 +130,7 @@ $amcharts_cdn = 'https://www.amcharts.com/lib/3';
 </div>
 <?php else: ?>
 <div class='widget' style="margin-top: 15px;">
-<div id="chartdiv"></div>
+<div id="container"></div>
 </div>
                     
 <table width="100%" cellpadding="0" cellspacing="0"><tr><td><h3>Trade History</h3></td><td align="right">( Last <?= count($trades_result) ?> trades )</td></tr></table>
@@ -152,236 +149,224 @@ $amcharts_cdn = 'https://www.amcharts.com/lib/3';
 <script type="text/javascript">
 createStockChart();
 
-function createStockChart() {
-    
-    var chart = AmCharts.makeChart( "chartdiv", {
-      "type": "stock",
-      "theme": "light",
-    
-      "dataSets": [ {
-        "fieldMappings": [ {
-          "fromField": "open",
-          "toField": "open"
-        }, {
-          "fromField": "close",
-          "toField": "close"
-        }, {
-          "fromField": "high",
-          "toField": "high"
-        }, {
-          "fromField": "low",
-          "toField": "low"
-        }, {
-          "fromField": "volume",
-          "toField": "volume"
-        }, {
-          "fromField": "avg",
-          "toField": "avg"
-        }, {
-          "fromField": "avg",
-          "toField": "value"
-        } ],
-        "color": "#7f8da9",
-        "title": "<?= $market_name ?>",
-        "categoryField": "period_start",
-        
-        "dataLoader": {
-          // we use csv instead of json because it is more compact over the wire.
-          "url": "api/hloc?market=<?= $market ?>&interval=day&timestamp=no&format=csv&endcaps=1",
-          "format": "csv",
-          "delimiter": ",",       // column separator
-          "useColumnNames": true, // use first row for column names
-          "skip": 1,               // skip header row
-          "reload": 300,           // auto reload every 5 minutes.
-          "timestamp": true       // add timestamp to url, to avoid caches.
-        }
-      } ],
-    
-      "panels": [ {
-          "title": "Price",
-          "showCategoryAxis": false,
-          "percentHeight": 70,
-          "valueAxes": [ {
-            "id": "v1",
-            "dashLength": 5
-          } ],
-    
-          "categoryAxis": {
-            "dashLength": 5,
-            "minPeriod": "hh",
-            "parseDates": true
-          },
-    
-          "stockGraphs": [ {
-            "type": "candlestick",
-            "id": "g1",
-            "openField": "open",
-            "closeField": "close",
-            "highField": "high",
-            "lowField": "low",
-            "valueField": "close",
-            "lineColor": "black",
-            "fillColors": "lightgreen",
-            "negativeLineColor": "black",
-            "negativeFillColors": "#db4c3c",
-            "fillAlphas": 1,
-            "useDataSetColors": false,
-            "comparable": false,
-            "compareField": "value",
-            "showBalloon": true,
-            "balloonText" : "Open: [[open]]\nClose: [[close]]\n\nHigh: [[high]]\nLow: [[low]]\nAvg: [[avg]]\n\nVolume: [[volume]]",
-            "proCandlesticks": false
-          } ],
-    
-          "stockLegend": {
-            "valueTextRegular": undefined,
-          }
-        },
-    
-        {
-          "title": "Volume",
-          "percentHeight": 30,
-          "marginTop": 1,
-          "showCategoryAxis": true,
-          "valueAxes": [ {
-            "dashLength": 5
-          } ],
-    
-          "categoryAxis": {
-            "dashLength": 5,
-            "parseDates": true
-          },
-    
-          "stockGraphs": [ {
-            "valueField": "volume",
-            "type": "column",
-            "showBalloon": true,
-            "fillAlphas": 1
-          } ],
-    
-          "stockLegend": {
-            "markerType": "none",
-            "markerSize": 0,
-            "labelText": "",
-            "periodValueTextRegular": "[[value.close]]"
-          }
-        }
-      ],  
-      
-      "chartScrollbarSettings": {
-        "graph": "g1",
-        "graphType": "line",
-        "usePeriod": "WW"
-      },
-    
-      "valueAxesSettings": {
-        "inside": false,
-        "autoMargins": true
-      },
-      
-      "chartCursorSettings": {
-        "valueLineBalloonEnabled": true,
-        "valueLineEnabled": true,
-      },
-    
-      "periodSelector": {
-        "position": "bottom",
-        "hideOutOfScopePeriods": true,
-        "periods": [
-<?php            
-    /*        
-        {
-          "period": "mm",
-          "count": 1,
-          "label": "Minute"
-        },
-*/
-?>
-        {
-          "period": "hh",
-          "count": 1,
-          "label": "Hour"
-        },
-        {
-          "period": "DD",
-          "count": 1,
-          "label": "Day"
-        }, {
-          "period": "WW",
-          "count": 1,
-          "label": "Week"
-        }, {
-          "period": "MM",
-          "selected": true,
-          "count": 1,
-          "label": "Month"
-        }, {
-          "period": "YYYY",
-          "count": 1,
-          "label": "Year"
-        }, {
-          "period": "YTD",
-          "label": "YTD"
-        }, {
-          "period": "MAX",
-          "label": "MAX"
-        } ]
-      },
-      "export": {
-        "enabled": true
-      },
 
-<?php    
-      /**
-       * this is an ugly hack to get the graph to display inside the div without
-       * clipping left and right edges, and also keep the graph from displaying
-       * on top of the left axis.  why this is necessary is beyond me.  I found
-       * it in an amcharts demo though. maybe an html/JS wizard can fix things
-       * so this is no longer necessary.
-       */
-?>      
-      "listeners": [{
-        "event": "init",
-        "method": function(e) {
-          // init
-          var margins = {
-            "left": 0,
-            "right": 20
-          };
-          
-          // iterate thorugh all of the panels
-          for ( var p = 0; p < chart.panels.length; p++ ) {
-            var panel = chart.panels[p];
-            
-            // iterate through all of the axis
-            for ( var i = 0; i < panel.valueAxes.length; i++ ) {
-              var axis = panel.valueAxes[ i ];
-              if ( axis.inside !== false ) {
-                continue;
-              }
-    
-              var axisWidth = axis.getBBox().width + 10;
-              if ( axisWidth > margins[ axis.position ] ) {
-                margins[ axis.position ] = axisWidth;
-              }
-            }
-            
-          }
-          
-          // set margins
-          if ( margins.left || margins.right ) {
-            chart.panelsSettings.marginLeft = margins.left;
-            chart.panelsSettings.marginRight = margins.right;
-            chart.invalidateSize();
-            // prevent redraw flashing.
-            window.setTimeout( function() {document.getElementById('chartdiv').style.visibility = 'visible'}, 500);
-          }
+function createStockChart() {
+
+/*                
+$(function () {
+//    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json&callback=?', function (data) {
+    $.getJSON('api/hloc?market=<?= $market ?>&timestamp=no&format=jscallback&callback=?', function (data) {
+
+//    $.getJSON('api/hloc/?market=dash_btc&callback=?', function (data) {
+
+        // split the data set into ohlc and volume
+        var ohlc = [],
+            volume = [],
+            dataLength = data.length,
+            // set the allowed units for data grouping
+            groupingUnits = [[
+                'week',                         // unit name
+                [1]                             // allowed multiples
+            ], [
+                'month',
+                [1, 2, 3, 4, 6]
+            ]],
+
+            i = 0;
+
+        for (i; i < dataLength; i += 1) {
+            ohlc.push([
+                data[i][0], // the date
+                data[i][1], // open
+                data[i][2], // high
+                data[i][3], // low
+                data[i][4]  // close
+            ]);
+
+            volume.push([
+                data[i][0], // the date
+                data[i][5]  // the volume
+            ]);
         }
-      }]    
-      
-    } );
-    
+
+
+        // create the chart
+        $('#container').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 1
+            },
+
+            title: {
+                text: 'AAPL Historical'
+            },
+
+            yAxis: [{
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'OHLC'
+                },
+                height: '60%',
+                lineWidth: 2
+            }, {
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'Volume'
+                },
+                top: '65%',
+                height: '35%',
+                offset: 0,
+                lineWidth: 2
+            }],
+
+            series: [{
+                type: 'candlestick',
+                name: 'AAPL',
+                data: ohlc,
+                dataGrouping: {
+                    units: groupingUnits
+                }
+            }, {
+                type: 'column',
+                name: 'Volume',
+                data: volume,
+                yAxis: 1,
+                dataGrouping: {
+                    units: groupingUnits
+                }
+            }]
+        });
+    });
+});                
+*/
+                
+$(function () {
+    /**
+     * Load new data depending on the selected min and max
+     */
+    function afterSetExtremes(e) {
+
+        var chart = $('#container').highcharts();
+
+        chart.showLoading('Loading data from server...');
+        $.getJSON('api/hloc?market=<?= $market ?>&milliseconds=true&timestamp=no&format=jscallback&callback=?&timestamp_from=' + Math.round(e.min) +
+                '&timestamp_to=' + Math.round(e.max), function (data) {
+
+            chart.series[0].setData(data);
+            chart.hideLoading();
+        });
+    }
+
+    // See source code from the JSONP handler at https://github.com/highcharts/highcharts/blob/master/samples/data/from-sql.php
+    $.getJSON('api/hloc?market=<?= $market ?>&milliseconds=true&timestamp=no&format=jscallback&callback=?', function (data) {
+
+        // Add a null value for the end date
+        // data = [].concat(data, [[Date.UTC(2011, 9, 14, 19, 59), null, null, null, null]]);
+
+        // create the chart
+        $('#container').highcharts('StockChart', {
+            chart : {
+                type: 'candlestick',
+                zoomType: 'x'
+            },
+            
+            plotOptions: {
+             candlestick: {
+                        color: 'red',
+                        upColor: 'green'
+                    }
+                },            
+
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Price (<?= $market_name ?>)'
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: 'black',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            },
+            navigator : {
+                adaptToUpdatedData: false,
+                series : {
+                    data : data
+                }
+            },
+
+            scrollbar: {
+                liveRedraw: false
+            },
+
+            title: {
+                text: '<?= $market_name ?> history'
+            },
+
+            subtitle: {
+                text: 'Displaying 1.7 million data points in Highcharts Stock by async server loading'
+            },
+
+            rangeSelector : {
+                buttons: [{
+                    type: 'hour',
+                    count: 1,
+                    text: '1h'
+                }, {
+                    type: 'day',
+                    count: 1,
+                    text: '1d'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: '1m'
+                }, {
+                    type: 'year',
+                    count: 1,
+                    text: '1y'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                inputEnabled: false, // it supports only days
+                selected : 2 // month
+            },
+
+            xAxis : {
+                events : {
+                    afterSetExtremes : afterSetExtremes
+                },
+                minRange: 3600 * 1000 // one hour
+            },
+
+            yAxis: {
+                floor: 0
+            },
+
+            series : [{
+                data : data,
+                dataGrouping: {
+                    enabled: false
+                }
+            }]
+        });
+    });
+});
 }
+
 </script>
 
 <?php endif; ?>

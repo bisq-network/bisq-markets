f90f9a29b2219c7f6f3208897235b55b635a45ab
Justin Carter <justin@misthos.io>
2019-10-26 23:20:55 +0200

    Add /markets query

--- src/main/java/bisq/markets/api/GraphQLQuery.java
+++ src/main/java/bisq/markets/api/GraphQLQuery.java
@@ -8,17 +8,18 @@ import java.util.*;
 import java.util.List;
 
 public abstract class GraphQLQuery {
-    public static final Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+    private static final Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
 
-    static public bisq.markets.api.GraphQLQuery forRequest(String path, Map<String,String> params) {
+    static bisq.markets.api.GraphQLQuery forRequest(String path, Map<String, String> params) {
         if (path.startsWith("/api/currencies")) {
-          return new CurrenciesQuery();
+            return new CurrenciesQuery();
+        }else if (path.startsWith("/api/markets")) {
+            return new MarketsQuery();
         } else {
             return null;
         }
     }
     public abstract Object translateResponse(String response);
-
 
     private static class CurrenciesQuery extends GraphQLQuery {
         private static class Currency {
@@ -43,7 +44,37 @@ public abstract class GraphQLQuery {
                 ret.put(currency.code,currency);
             }
             return ret;
+        }
+    }
 
+    private static class MarketsQuery extends GraphQLQuery {
+        private static class Market {
+            String pair;
+            String lname;
+            String rname;
+            String lsymbol;
+            String rsymbol;
+            int lprecision;
+            int rprecision;
+            String ltype;
+            String rtype;
+        }
+        private static final String marketsQuery = "{ markets { pair lname: lName rname: rName lsymbol: lSymbol lprecision: lPrecision rsymbol: rSymbol rprecision: rPrecision ltype: lTypeLowerCase rtype: rTypeLowerCase } }";
+        private final String query;
+        private MarketsQuery() {
+            query = marketsQuery;
+        }
+
+        @Override
+        public Object translateResponse(String response) {
+            GraphQLResponse<List<Market>> markets = gson.fromJson(response,new TypeToken<GraphQLResponse<ArrayList<Market>>>(){}.getType());
+            Iterator<Market> iter = markets.getData().iterator();
+            Map<String,Market> ret = new HashMap();
+            while(iter.hasNext()) {
+                Market market = iter.next();
+                ret.put(market.pair,market);
+            }
+            return ret;
         }
     }
 }
